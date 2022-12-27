e21cc59177f85261d2715b4067ff7d1e8213a04f
devlanger <langer.msg@gmail.com>
2022-12-27 13:25:24 +0100

    Initial Structure Commit

---
+++ Persistence/DbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+	public class DbContext : IdentityDbContext<AppUser>
+	{
+		public DbContext(DbContextOptions<DbContext> options) : base(options)
+		{
+
+		}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            //TODO: Automapper etc...
+
+        }
+    }
+}
+
